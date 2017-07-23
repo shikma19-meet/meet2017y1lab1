6f0777673fa7a39f09937c4f89f8bd06b3bce4da
shikma19-meet <shikma19@meet.mit.edu>
2017-07-23 19:04:42 +0300

    Stopping here

---
+++ MEETinturtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+turtle.penup()
+turtle.goto (-200+275,-100)
+turtle.pendown()
+turtle.goto(75-150,-100)
+turtle.mainloop()
